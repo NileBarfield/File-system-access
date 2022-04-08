a177f78c376602939fe0ae19695880fc377a3728
NileBarfield <nile.barfield@gmail.com>
2022-04-08 09:37:01 -0500

    commiting progress, can now display different files through created buttons, files do not get duplicated

---
+++ xlsxConverter.py
@@ -0,0 +1,4 @@
+#testing pandas in order to be able to convert xlsx and xls files to readable format or format them in to csv file
+import pandas as pd
+import numpy as np
+df = pd.read_csv('Top1KByAvgRating_Actors-Directors.csv')
